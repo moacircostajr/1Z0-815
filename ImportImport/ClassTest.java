dcfe3fd3374006bc22042fb7b6674be662a67046
Moacir Costa <moacircostajr@gmail.com>
2021-09-26 12:28:12 -0300

    test: psvm running psvm

--- ImportImport/ClassTest.java
+++ ImportImport/ClassTest.java
@@ -2,11 +2,10 @@
 // add any necessary import statements here
 
 public class ClassTest {
-  public static void main() {   // error: 'main' method is not declared 'public static'
+  public static void main(String[] args) { // error: 'main' method is not declared 'public static'
     // add test code here
     System.out.println("Hello World Class Test");
     TestClass.main(null);
     // add any print statements here
   }
 }
-
--- ImportImport/TestClass.java
+++ ImportImport/TestClass.java
@@ -2,10 +2,10 @@
 // add any necessary import statements here
 
 class TestClass {
-  public static void main(String[] args) {   // error: 'main' method is not declared 'public static'
+  public static void main(String[] args) { // error: 'main' method is not declared 'public static'
     // add test code here
     System.out.println("Hello World");
-    ClassTest.main();
+    ClassTest.main(null);
     // add any print statements here
   }
 }
