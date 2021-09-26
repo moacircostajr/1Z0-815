9e85054a8ede51d2e6afcec9edfa8117d929353a
Moacir Costa <moacircostajr@gmail.com>
2021-09-26 19:53:23 -0300

    test: double literal at scientific notation

---
+++ TestaDouble.java
@@ -0,0 +1,6 @@
+public class TestaDouble {
+    public static void main(String[] args) {
+        double maxDoubleValue = 1.7976931E+308; // max double value at scientific notation
+        System.out.printf("%.7e\n", maxDoubleValue); // 7 casas depois da vírgula
+    }
+}
